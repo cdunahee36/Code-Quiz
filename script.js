144513192f7112e8045976919100ca8555330480
cdunahee36 <dunahee10@gmail.com>
2020-10-18 20:21:04 -0500

    I added a script file and created an array within it.

---
+++ script.js
@@ -0,0 +1,9 @@
+var page = document.body
+var javaQuestions = ["What are the components of conditionals in Javascript?",
+"Which do you use to take a value and display it on the page?", "What is Supermans weakness?",
+"What year was the movie Titanic released?", "Who was spiderman in the origional spiderman movie?"];
+
+
+
+
+
