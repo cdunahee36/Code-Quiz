77eed425c6aee758c487343f88df2ac2a517feaa
cdunahee36 <dunahee10@gmail.com>
2020-10-21 16:46:07 -0500

    Added timer to the quiz and cleaned more code.

--- questions.js
+++ questions.js
@@ -8,37 +8,37 @@
 */
 var questionText = document.getElementById("questionText");
 var choices = document.querySelector('.choices');
+var timeEl = document.querySelector('.time');
 var currentQuestionIndex = 0;
 
-
+setTime();
 //List of questions for the quiz
 
 const questions = [{
-   title: "Supermans only weakness is:",
+   title: "Supermans only weakness is?",
    choices: ["Kryptonite","Dogs","Icecream","Milk"],
    answer: "Kryptonite" 
 },
 {
-   title: "What is not javascript:",
+   title: "What is not javascript?",
    choices: ["Javascript","Javascript","Javascript","Javascraps"],
    answer: "Javascraps"
 },
 {
-   title: "What does the append function do in javascript:",
+   title: "What does the append function do in javascript?",
    choices: ["removes valuse","adds values","subtracts values","multipys values"],
    answer: "adds values"
 },
 {
-   title: "What year was Barack obama elected president:",
+   title: "What year was Barack obama elected president?",
    choices: ["2004","2006","2008","2010"],
    answer: "2008"
 },
 {
-   title: "When did the Cubs finally win the world series:",
+   title: "When did the Cubs finally win the world series?",
    choices: ["2014","2008","2018","2016"],
    answer: "2016"
 }];
-
 
 
 
@@ -55,11 +55,13 @@ function generatedChoicesList() {
        var button = document.createElement("button");
        button.textContent = currentQuestion.choices[i];
        console.log("adding choie " + choiceText);
+
        button.addEventListener("click", function() {
          console.log(button.textContent);
          moveOn();
          attemptAnswer(button.textContent);
        }, false);
+
        lChoices.appendChild(button);
        qList.appendChild(lChoices);
    }
@@ -74,11 +76,24 @@ function moveOn() {
    refreshScreen();
 }
 
+var secondsLeft = 60;
+
+function setTime() {
+   var timerInterval = setInterval(function() {
+     secondsLeft--;
+     timeEl.textContent = "Timer" + " " + secondsLeft;
+ 
+     if(secondsLeft === 0) {
+       clearInterval(timerInterval);
+     }
+ 
+   }, 1000);
+ }
+
 
 
 function attemptAnswer(answerString) {
    console.log("Selected " + answerString);
-
 }
 
 function refreshScreen() {
